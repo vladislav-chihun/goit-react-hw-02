87c7a42f174621c0e00498f3c42efd65212abfa3
vladislav-chihun <vladikkosmo@gmail.com>
2024-05-08 11:00:56 +0200

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,8 +1,8 @@
 
 export default function Feedback({ good, neutral, bad }) {
     return (<>
-        <p>Good:<span>{good}</span></p>
-        <p>Neutral:<span>{neutral}</span></p>
-        <p>Bad:<span>{bad}</span></p>
+        <p><span>{good}</span></p>
+        <p><span>{neutral}</span></p>
+        <p><span>{bad}</span></p>
     </>)
 }
