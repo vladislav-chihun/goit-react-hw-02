404a71cb40c6154fbcf1a37190b38bfe0a4fa013
vladislav-chihun <vladikkosmo@gmail.com>
2024-05-09 12:30:55 +0200

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,5 +4,7 @@ export default function Feedback({ feedbackType:{good, neutral, bad},totalFeedba
         <p>Good:<span>{good}</span></p>
         <p>Neutral:<span>{neutral}</span></p>
         <p>Bad:<span>{bad}</span></p>
+        <p>Total:<span>{totalFeedback}</span></p>
+        <p>Positive:<span>{Math.round((good / totalFeedback) * 100)}%</span></p>
     </>)} 
 }
